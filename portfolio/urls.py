d27a1cf224a552dd06945ad42ef6f5684c80c0ca
Bhavesh Shah <bhavrish@gmail.com>
2019-09-13 18:36:27 -0400

    Jobs app up and running

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static # static is for image, video, etc. files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # connects images in model to media path
